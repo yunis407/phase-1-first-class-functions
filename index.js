f2b21b839ad533499053310ecbf58cd2468fed20
yunisaliosman <yunis.osman@student.moringaschool.com>
2023-02-13 15:42:37 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 function receivesAFunction(callback) {
     return callback();
     }
-    
     function returnsANamedFunction(){
         return function greeting(){
             console.log('Hello world');
         }
         return greeting();
     }
-    
     function returnsAnAnonymousFunction(){
         return function(string){
             console.log(string);
